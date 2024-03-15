9564ec378db28bb51777d14c0908705e23ca416b
luckaasf <125568601+luckaasf@users.noreply.github.com>
2024-03-15 12:46:53 +0000

    move start node and finish node by click and drag

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -8,32 +8,98 @@ import MyContext from "./MyContext";
 const GRID_ROW_SIZE = 22;
 const GRID_COL_SIZE = 67;
 
-const START_NODE_ROW = 11;
-const START_NODE_COL = 20;
-const FINISH_NODE_ROW = 11;
-const FINISH_NODE_COL = 40;
+
+const START_NODE_POSITION = { row: 11, col: 20 };
+const FINISH_NODE_POSITION = { row: 11, col: 40};
 
 function Grid() {
     const [grid, setGrid] = useState([]);
     const [mouseIsPressed, setMouseIsPressed] = useState(false);
+    const [nodePressed, setNodePressed] = useState(null);
+    const [startNodePosition, setStartNodePosition] = useState(START_NODE_POSITION);
+    const [finishNodePosition, setFinishNodePosition] = useState(FINISH_NODE_POSITION);
 
     const { config } = useContext(MyContext);
 
     function handleOnMouseDown(row, col) {
-        if ((grid[row][col].isStart) || (grid[row][col].isFinish)) return;
-        const newGrid = toggleWallGrid(grid, row, col);
-        setGrid(newGrid);
-        setMouseIsPressed(true);
+        const node = grid[row][col];
+        if (node.isStart || node.isFinish) {
+            setNodePressed(node);
+            setMouseIsPressed(true);
+        } else {
+            const newGrid = toggleWallGrid(grid, row, col);
+            setGrid(newGrid);
+            setMouseIsPressed(true);
+        }
     }
 
     function handleOnMouseEnter(row, col) {
         if (!mouseIsPressed || (grid[row][col].isStart) || (grid[row][col].isFinish) || grid[row][col].isWall) return;
-        const newGrid = toggleWallGrid(grid, row, col);
-        setGrid(newGrid);
+        if (nodePressed) {
+            const newGrid = moveNode(grid, row, col, nodePressed.row, nodePressed.col);
+            setGrid(newGrid);
+            setNodePressed({ ...nodePressed, row: row, col: col });
+            
+            if (nodePressed.isStart) {
+                setStartNodePosition( {row: row, col: col} );
+            } else if (nodePressed.isFinish) {
+                setFinishNodePosition( {row: row, col: col} );
+            }
+            
+        } else {
+            const newGrid = toggleWallGrid(grid, row, col);
+            setGrid(newGrid);
+        }
     }
 
     function handleOnMouseUp() {
         setMouseIsPressed(false);
+        setNodePressed(null);
+    }
+
+    function moveNode(grid, newRow, newCol, oldRow, oldCol) { 
+        const newGrid = grid.map(row => row.slice());
+        const oldNode = newGrid[oldRow][oldCol];
+        const newNode = { ...oldNode, row: newRow, col: newCol };
+        newGrid[oldRow][oldCol] = { ...grid[oldRow][oldCol], isStart: false, isFinish: false };
+        newGrid[newRow][newCol] = newNode;
+        return newGrid;
+    }
+    
+    function initializeGrid() {
+        const grid = [];
+        for (let row = 0; row < GRID_ROW_SIZE; row++) {
+            const currentRow = [];
+            for (let col = 0; col < GRID_COL_SIZE; col++) {
+                currentRow.push(createNode(row, col));
+            }
+            grid.push(currentRow);
+        }
+        return grid;
+    }
+    
+    function createNode(row, col) {
+        return {
+            row,
+            col,
+            isVisited: false,
+            isWall: false,
+            isStart: row === startNodePosition.row && col === startNodePosition.col,
+            isFinish: row === finishNodePosition.row && col === finishNodePosition.col,
+            distance: Infinity,
+            heuristic: 0,
+            previousNode: null,
+        };
+    }
+    
+
+    
+    function toggleWallGrid(grid, row, col) {
+        const newGrid = grid.slice();
+        const oldNode = newGrid[row][col];
+        const newNode = { ...oldNode, isWall: !oldNode.isWall };
+        newGrid[row][col] = newNode;
+        return newGrid;
     }
 
     useEffect(() => {
@@ -57,7 +123,7 @@ function Grid() {
             document.removeEventListener("mouseup", handleDocumentMouseUp);
         };
     }, []);
-
+    
     function animatePathFinder(visitedNodes, shortestPathNodes) {
         console.log("visited nodes: ", visitedNodes);
         console.log("shortestPath: ", shortestPathNodes);
@@ -67,25 +133,25 @@ function Grid() {
                     animateShortestPath(shortestPathNodes);
                 }
                 const currentNode = visitedNodes[i];
-                if (!((currentNode.row === START_NODE_ROW && currentNode.col === START_NODE_COL) || (currentNode.row === FINISH_NODE_ROW && currentNode.col === FINISH_NODE_COL)))
+                if (!((currentNode.row === startNodePosition.row && currentNode.col === startNodePosition.col) || (currentNode.row === finishNodePosition.row && currentNode.col === finishNodePosition.col)))
                     document.getElementById(`node-${currentNode.row}-${currentNode.col}`).className="node node-visited";
             }, 10 * i);
         }
     }
-
+    
     function animateShortestPath(shortestPathNodes) {
         for (let i = 0; i < shortestPathNodes.length; i++) {
             setTimeout(() => {
                 const currentNode = shortestPathNodes[i];
-                if (!((currentNode.row === START_NODE_ROW && currentNode.col === START_NODE_COL) || (currentNode.row === FINISH_NODE_ROW && currentNode.col === FINISH_NODE_COL)))
-                    document.getElementById(`node-${currentNode.row}-${currentNode.col}`).className="node node-shortest-path";
-            }, 35 * i);
+                if (!((currentNode.row === startNodePosition.row && currentNode.col === startNodePosition.col) || (currentNode.row === finishNodePosition.row && currentNode.col === finishNodePosition.col)))
+                document.getElementById(`node-${currentNode.row}-${currentNode.col}`).className="node node-shortest-path";
+        }, 35 * i);
         }
     }
 
     function handleVisualizeButton() {
-        const startNode = grid[START_NODE_ROW][START_NODE_COL]; 
-        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+        const startNode = grid[startNodePosition.row][startNodePosition.col]; 
+        const finishNode = grid[finishNodePosition.row][finishNodePosition.col];
         const algorithmSelected = config.algorithm;
         const { visitedNodes, shortestPathNodes }= runAlgorithm(algorithmSelected, startNode, finishNode);
         animatePathFinder(visitedNodes, shortestPathNodes);
@@ -102,10 +168,10 @@ function Grid() {
                 const shortestPathNodes = dijkstraPath(finishNode); 
                 return { visitedNodes: visitedNodesDijkstra, shortestPathNodes: shortestPathNodes };
             default:
-                break;
+            break;
         }
     }
-
+                
     return (
         <>
             <div className="grid">
@@ -136,38 +202,6 @@ function Grid() {
     );
 }
 
-function initializeGrid() {
-    const grid = [];
-    for (let row = 0; row < GRID_ROW_SIZE; row++) {
-        const currentRow = [];
-        for (let col = 0; col < GRID_COL_SIZE; col++) {
-            currentRow.push(createNode(row, col));
-        }
-        grid.push(currentRow);
-    }
-    return grid;
-}
 
-function createNode(row, col) {
-    return {
-        row,
-        col,
-        isVisited: false,
-        isWall: false,
-        isStart: row === START_NODE_ROW && col === START_NODE_COL,
-        isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
-        distance: Infinity,
-        heuristic: 0,
-        previousNode: null,
-    };
-}
-
-function toggleWallGrid(grid, row, col) {
-    const newGrid = grid.slice();
-    const oldNode = newGrid[row][col];
-    const newNode = { ...oldNode, isWall: !oldNode.isWall };
-    newGrid[row][col] = newNode;
-    return newGrid;
-}
 
 export default Grid;
