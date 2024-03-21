8f1e9ace89504f2140132f5b158c9628a8f4ec3e
luckaasf <125568601+luckaasf@users.noreply.github.com>
2024-03-21 23:03:42 +0000

    code refactor signup

--- backend/pathfinder/serializers.py
+++ backend/pathfinder/serializers.py
@@ -8,16 +8,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'username', 'password', 'confirm_password']
-        extra_kwargs={'password':{'write_only':True}}
-    
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError({'error': 'Username already exists'})
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({'error': 'Email already exists'})
+        if len(data['username']) < 4:
+            raise serializers.ValidationError({'error': 'Username should have at least 4 characters'})
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({'error': 'Passwords are not matching'})
+        return data
+
     def create(self, validated_data):
-        validated_data.pop('confirm_password') # remove from the dictionary because its not needed to create a new user
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-        user.set_password(validated_data['password'])
-        
-        user.save()
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        CustomUser.objects.create(user=user, username=validated_data['username'], email=validated_data['email'])
         return user
         
